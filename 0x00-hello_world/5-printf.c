4c7093087ff3e6862db265e9ebbaa0cd0a0ec07a
iamclement1 <iamnnamdiclement@gmail.com>
2023-03-16 14:19:42 -0700

    fixex

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,12 @@
-#inlude <stdio.h>
+#include <stdio.h>
+
 /**
- * Main entry point
- * 
- * /
- int main(void){
- printf("with proper grammer, but the outcome is a piece of art,\n");
- return (0);
- }
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
