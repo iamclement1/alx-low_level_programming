ce61c4fd8f853a1f68fcdadf52a5b01ed3893221
iamclement1 <iamnnamdiclement@gmail.com>
2023-03-17 08:57:46 -0700

    fixed the spacing and compiled code

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,28 +1,37 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
 /**
- * main - Prints the last digit of a randomly generated
- * number and whether it is greater than 5, less than 6 or 0.
+ * main - Prints the last digit of a randomly generated number
+ * and whether it is greater than 5, less than 6, or 0.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
 	int n;
-	int lastNum;
-	char str[] = "Last digit of";
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-       lastNum = n % 10;
 
-	if (lastNum > 5)
-		printf("%s %d is %d and is greater than 5\n", str, n, lastNum);
-	else if (lastNum == 0)
-		printf("%s %d is %d and is 0\n", str, n, lastNum);
-	else (lastNum < 6)
-		printf("%s %d is %d and is less than 6 and not 0\n", str, n, lastNum);
+	if ((n % 10) > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n",
+			n, n % 10);
+	}
+	else if ((n % 10) < 6 && (n % 10) != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+			n, n % 10);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is 0\n",
+			n, n % 10);
+	}
 
 	return (0);
+
 }
+
