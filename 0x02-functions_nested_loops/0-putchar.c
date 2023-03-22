4d325b2dc1a40aa681cfd0bfa83d392fafa6ad40
iamclement1 <iamnnamdiclement@gmail.com>
2023-03-21 17:00:13 -0700

    print 0;

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - The program should return 0
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{	int sh = 0;	
+
+	while (sh = 0)
+	{
+		putchar('\n');
+		sh++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
