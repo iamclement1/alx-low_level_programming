5b0423e2510fba570e79499dd9c4d3b05e7fa2f6
iamclement1 <iamnnamdiclement@gmail.com>
2023-03-27 14:37:02 -0700

    a function that changes two numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap int - swap two numbers;
+ * @a: first number
+ * @b: second number
+ * Return : (0)
+ */
+void swap_int(int *a, int *b)
+{
+	int temp = a;
+
+	*a = *b;
+	*b = temp;
+}
